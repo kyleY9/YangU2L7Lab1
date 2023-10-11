7974a5e21bc74f3da5de68d123a8e447f27ed3d4
kyleY9 <KyleY9@nycstudents.net>
2023-10-11 12:36:14 -0400

    hiya!

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        String origStr = "Hello how are you today?";
-        String str9 = origStr.substring(6,9);
-        System.out.println(str9);
+        String str1 = "KFC";
+        String str2 = "Popeyes";
+        if (str1.equals(str2)) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
     }
 }
